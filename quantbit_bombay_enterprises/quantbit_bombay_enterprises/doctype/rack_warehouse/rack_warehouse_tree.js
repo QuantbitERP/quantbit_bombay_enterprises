c52053e5b3819f61c85aeed48b4b5225ab9ff72f
tejalkumbhar1611 <tejal.kumbhar@erpdata.in>
2026-08-13 13:25:48 +0530

    13 aug 2026 changes for rack

--- quantbit_bombay_enterprises/quantbit_bombay_enterprises/doctype/rack_warehouse/rack_warehouse_tree.js
+++ quantbit_bombay_enterprises/quantbit_bombay_enterprises/doctype/rack_warehouse/rack_warehouse_tree.js
@@ -1,8 +1,5 @@
 // Copyright (c) 2026, Quantbit Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Warehouse Location", {
-// 	refresh(frm) {
-
-// 	},
-// });
+// frappe.treeview_settings["Rack Warehouse"] = {
+// };
