6097c589b8b4c10923c2037b735d9e03fcc0f766
tejalkumbhar1611 <tejal.kumbhar@erpdata.in>
2026-09-09 16:45:53 +0530

    9 sep 2026 changes for HDT-26-05021

--- quantbit_bombay_enterprises/hooks.py
+++ quantbit_bombay_enterprises/hooks.py
@@ -256,3 +256,12 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures = [
+
+    {
+    "dt": "Property Setter",
+    "filters": [
+        ["module", "=", "Quantbit Bombay Enterprises"]
+    ]
+    },
+]
